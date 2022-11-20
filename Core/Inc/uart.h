d6add2986d05bc7fab2901d00f5c031fd2ef42eb
Rsulinski <radoslaw.sulinski@gmail.com>
2022-11-20 18:59:39 +0100

    Initial commit - .ioc, .h and .c files

---
+++ Core/Inc/uart.h
@@ -0,0 +1,15 @@
+/*
+ * uart.h
+ *
+ *  Created on: Nov 20, 2022
+ *      Author: Radoslaw Sulinski
+ */
+
+#ifndef SRC_UART_H_
+#define SRC_UART_H_
+// Nothing here yet, just checking if this file won't get deleted on code generation
+
+
+
+
+#endif /* SRC_UART_H_ */
